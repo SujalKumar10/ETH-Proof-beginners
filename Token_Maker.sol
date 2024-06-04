ce3824163b2deff6740c3480ebb8d424e3ddeaf6
SujalKumar10 <146089340+SujalKumar10@users.noreply.github.com>
2024-06-04 17:37:45 +0530

    Create Token_Maker.sol

---
+++ Token_Maker.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract MyToken {
+
+    string public Token_Namre = "Berry";
+    string public Token_Abbrv = "BRY";
+    uint public Total_Supply;
+
+    mapping(address => uint) public balances;
+
+   function mint(address Address, uint value) public {
+      Total_Supply += value;
+      balances[Address] +=value;
+    }
+
+    function burn(address Address, uint value) public {
+      if(balances[Address]>= value) {
+      Total_Supply -= value;
+      balances[Address] -=value;
+      }
+      
+    }
+
+}
